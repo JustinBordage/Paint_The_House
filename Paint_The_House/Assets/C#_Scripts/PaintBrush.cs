6d164c864b89c8d20923cee52a599ecc5773550b
JBordage <38659460+JustinBordage@users.noreply.github.com>
2025-01-28 17:15:44 -0500

    Refactored the paint brush script to allow keyboard inputs for testing

--- Paint_The_House/Assets/C#_Scripts/PaintBrush.cs
+++ Paint_The_House/Assets/C#_Scripts/PaintBrush.cs
@@ -10,6 +10,8 @@ public class PaintBrush : MonoBehaviour
     public int deadzone = 5;
     public Color paintColor { get; private set; }
 
+    private Vector2 lastDir = Vector2.zero;
+    
     private static AudioPlayer audioPlayer;
 
     void Awake()
@@ -33,20 +35,46 @@ public class PaintBrush : MonoBehaviour
         {
             //Translates the brush if the swipe is long enough
             if (Input.touchCount > 0)
+            {
                 GetBrushDestination(Input.GetTouch(0).deltaPosition);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow) ||
+                    Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    var xAxis = Input.GetAxis("Horizontal");
+                    var yAxis = Input.GetAxis("Vertical");
+                    var dir = new Vector2(xAxis, yAxis).normalized * deadzone;
+                    
+                    if (lastDir.x != dir.x || lastDir.y != dir.y)
+                    {
+                        var didMove = GetBrushDestination(dir);
+
+                        if (didMove)
+                        {
+                            lastDir = dir;
+                        }
+                        else
+                        {
+                            lastDir = Vector2.zero;
+                        }
+                    }
+                }
+            }
         }
     }
 
-    private void GetBrushDestination(Vector2 dir)
+    private bool GetBrushDestination(Vector2 dir)
     {
         //Calculates the absolute value of the
         Vector2 absDir = new Vector2(Mathf.Abs(dir.x), Mathf.Abs(dir.y));
-
+        
         //Exits if no movement will occur or if not above the deadzone
-        if ((absDir.x < deadzone && absDir.y < deadzone)
-            || absDir.x == absDir.y)
-            return;
-
+        if ((absDir.x < deadzone && absDir.y < deadzone) || absDir.x == absDir.y)
+        {
+            return false;
+        }
 
         //Checks for the larger axis, if
         //both the same(dir == Vector2.Zero)
@@ -59,31 +87,36 @@ public class PaintBrush : MonoBehaviour
 
         //Checks for obstacles and map edges
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, dir, out hit, 10f, 1 << 11))
+        if (!Physics.Raycast(transform.position, dir, out hit, 10f, 1 << 11))
         {
-            //Fetches the current and obstacle positions
-            Vector2 currPos = transform.position;
-            Vector2 obstaclePos = hit.collider.transform.position;
-
-            //Calculates the distance to travel
-            Vector3 translateBy = (obstaclePos - currPos) - dir;
-            translateBy.x *= Mathf.Abs(dir.x);
-            translateBy.y *= Mathf.Abs(dir.y);
-
-            //Calculates the distance to the destination
-            dest = transform.position + translateBy;
-            float distance = VectorExt.VertDist(dest, transform.position);
-
-            //Triggers the Translation
-            if(distance > 0f)
-            {
-                isMoving = true;
-                audioPlayer.PlaySFX("BrushSwoosh");
-                prevDistance = distance;
-                distTraveled = 0f;
-            }
-
+            return false;
         }
+        
+        //Fetches the current and obstacle positions
+        Vector2 currPos = transform.position;
+        Vector2 obstaclePos = hit.collider.transform.position;
+
+        //Calculates the distance to travel
+        Vector3 translateBy = (obstaclePos - currPos) - dir;
+        translateBy.x *= Mathf.Abs(dir.x);
+        translateBy.y *= Mathf.Abs(dir.y);
+
+        //Calculates the distance to the destination
+        dest = transform.position + translateBy;
+        float distance = VectorExt.VertDist(dest, transform.position);
+        
+        if(distance <= 0f)
+        {
+            return false;
+        }
+        
+        //Triggers the Translation
+        isMoving = true;
+        audioPlayer.PlaySFX("BrushSwoosh");
+        prevDistance = distance;
+        distTraveled = 0f;
+        
+        return true;
     }
 
     private void TranslateBrush()
@@ -104,6 +137,7 @@ public class PaintBrush : MonoBehaviour
             distTraveled += 0.05f;
             transform.position = dest;
             WallManager.mInstance.CheckWall(this);
+            lastDir = Vector2.zero;
         }
 
         //Sometimes the brush moves too fast
